e5f2a4ac393de9fae245b92585ff2e44839198ba
Omolola <omololaagbaje6@gmail.com>
2022-09-08 17:44:47 -0700

    Write a C program that prints exact

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,17 @@
-#include < stdio.h >
+#include <stdio.h>
+
+
 /**
- *  main - Entry point
+ *main - Entry point
  *
- *  Write a script that runs a C file through the preprocessor and save result
- *
- *  Return: Always 0 (Success)
+ *Return: Always 0 (Success)
  */
 
 int main(void)
 
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
+}
