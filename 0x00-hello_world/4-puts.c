b0188885d3219924fc23823e018e37c6afb04eb4
Omolola <omololaagbaje6@gmail.com>
2022-09-08 17:17:12 -0700

    Write a C program that prints exactl

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include < stdio.h >
+/**
+ *  main - Entry point
+ *
+ *  Write a script that runs a C file through the preprocessor and save result
+ *
+ *  Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
